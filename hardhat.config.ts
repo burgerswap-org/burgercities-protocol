d030ae4ca07a26cd5f32c80e5faafaa52ae410f1
tdergouziv2 <tdergouzi@gmail.com>
2024-01-17 16:44:23 +0800

    Feat: add blast contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,16 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- import { HardhatUserConfig } from "hardhat/types";
- import '@openzeppelin/hardhat-upgrades';
- 
- import "@nomiclabs/hardhat-waffle";
- import "@nomiclabs/hardhat-etherscan";
- import "hardhat-typechain";
- import fs from "fs";
- import path from "path";
- const USER_HOME = process.env.HOME || process.env.USERPROFILE
- let data = {
+import { HardhatUserConfig } from "hardhat/types";
+import '@openzeppelin/hardhat-upgrades';
+
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-typechain";
+import fs from "fs";
+import path from "path";
+const USER_HOME = process.env.HOME || process.env.USERPROFILE
+let data = {
   "PrivateKey": "",
   "InfuraApiKey": "",
   "EtherscanApiKey": "",
@@ -18,126 +18,130 @@
   "MaticscanApiKey": "",
   "BscscanApiKey": "",
 };
- 
- let filePath = path.join(USER_HOME+'/.hardhat.data.json');
- if (fs.existsSync(filePath)) {
-   let rawdata = fs.readFileSync(filePath);
-   data = JSON.parse(rawdata.toString());
- }
- filePath = path.join(__dirname, `.hardhat.data.json`);
- if (fs.existsSync(filePath)) {
-   let rawdata = fs.readFileSync(filePath);
-   data = JSON.parse(rawdata.toString());
- }
- 
- const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
-   version: "0.8.9",
-   settings: {
-     optimizer: {
-       enabled: true,
-       runs: 200,
-     },
-     metadata: {
-       bytecodeHash: 'none',
-     },
-   },
- }
- 
- const LOWER_OPTIMIZER_COMPILER_SETTINGS = {
-   version: "0.8.9",
-   settings: {
-     optimizer: {
-       enabled: true,
-       runs: 10_000,
-     },
-   },
- }
- 
- const DEFAULT_COMPILER_SETTINGS = {
-   version: "0.8.9",
-   settings: {
-     optimizer: {
-       enabled: true,
-       runs: 1_000_000,
-     }
-   },
- }
- 
- const config: HardhatUserConfig = {
-   defaultNetwork: "hardhat",
-   solidity: {
-     compilers: [DEFAULT_COMPILER_SETTINGS],
-     overrides: {
-       'contracts/HeroManage.sol': LOWER_OPTIMIZER_COMPILER_SETTINGS,
-     },
-   },
-   networks: {
-     hardhat: {},
-     mainnet: {
-       url: `https://mainnet.infura.io/v3/${data.InfuraApiKey}`,
-       accounts: [data.PrivateKey]
-     },
-     goerli: {
-       url: `https://goerli.infura.io/v3/${data.InfuraApiKey}`,
-       accounts: [data.PrivateKey]
-     },
-     sepolia: {
-       url: `https://sepolia.infura.io/v3/${data.InfuraApiKey}`,
-       accounts: [data.PrivateKey]
-     },
-     bsctestnet: {
-       url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
-       accounts: [data.PrivateKey]
-     },
-     bscmainnet: {
-       url: `https://rpc.ankr.com/bsc`,
-       accounts: [data.PrivateKey]
-     },
-     opbnbtestnet: {
+
+let filePath = path.join(USER_HOME + '/.hardhat.data.json');
+if (fs.existsSync(filePath)) {
+  let rawdata = fs.readFileSync(filePath);
+  data = JSON.parse(rawdata.toString());
+}
+filePath = path.join(__dirname, `.hardhat.data.json`);
+if (fs.existsSync(filePath)) {
+  let rawdata = fs.readFileSync(filePath);
+  data = JSON.parse(rawdata.toString());
+}
+
+const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
+  version: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
+
+const LOWER_OPTIMIZER_COMPILER_SETTINGS = {
+  version: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 10_000,
+    },
+  },
+}
+
+const DEFAULT_COMPILER_SETTINGS = {
+  version: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    }
+  },
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [DEFAULT_COMPILER_SETTINGS],
+    overrides: {
+      'contracts/HeroManage.sol': LOWER_OPTIMIZER_COMPILER_SETTINGS,
+    },
+  },
+  networks: {
+    hardhat: {},
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${data.InfuraApiKey}`,
+      accounts: [data.PrivateKey]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${data.InfuraApiKey}`,
+      accounts: [data.PrivateKey]
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${data.InfuraApiKey}`,
+      accounts: [data.PrivateKey]
+    },
+    bsctestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: [data.PrivateKey]
+    },
+    bscmainnet: {
+      url: `https://rpc.ankr.com/bsc`,
+      accounts: [data.PrivateKey]
+    },
+    opbnbtestnet: {
       url: `https://opbnb-testnet-rpc.bnbchain.org`,
       accounts: [data.PrivateKey]
     },
-     opbnbmainnet: {
+    opbnbmainnet: {
       url: `https://opbnb-mainnet-rpc.bnbchain.org`,
       accounts: [data.PrivateKey]
     },
-     arbitrum_goerli: {
-       url: `https://arbitrum-goerli.infura.io/v3/${data.InfuraApiKey}`,
-       accounts: [data.PrivateKey],
-       gas: "auto",
-     },
-     arbitrum: {
-       url: `https://arbitrum-mainnet.infura.io/v3/${data.InfuraApiKey}`,
-       accounts: [data.PrivateKey],
-     },
-     mantatestnet: {
+    arbitrum_goerli: {
+      url: `https://arbitrum-goerli.infura.io/v3/${data.InfuraApiKey}`,
+      accounts: [data.PrivateKey],
+      gas: "auto",
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${data.InfuraApiKey}`,
+      accounts: [data.PrivateKey],
+    },
+    mantatestnet: {
       url: `https://pacific-rpc.testnet.manta.network/http`,
       accounts: [data.PrivateKey]
     },
-     mantamainnet: {
+    mantamainnet: {
       url: `https://pacific-rpc.manta.network/http`,
       accounts: [data.PrivateKey]
     },
-   },
-   etherscan: {
-      apiKey: {
-        mainnet: data.EtherscanApiKey,
-        goerli: data.EtherscanApiKey,
-        arbitrumOne: data.ArbitrumscanApiKey,
-        polygon: data.MaticscanApiKey,
-        bsc: data.BscscanApiKey,
-        bscTestnet: data.BscscanApiKey,
+    blasttestnet: {
+      url: `https://sepolia.blast.io`,
+      accounts: [data.PrivateKey]
     }
-   },
-   paths: {
-     sources: "./contracts",
-     tests: "./test",
-     cache: "./cache",
-     artifacts: "./artifacts"
-   },
-   mocha: {
-     timeout: 100000
-   }
- };
- 
- export default config;
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: data.EtherscanApiKey,
+      goerli: data.EtherscanApiKey,
+      arbitrumOne: data.ArbitrumscanApiKey,
+      polygon: data.MaticscanApiKey,
+      bsc: data.BscscanApiKey,
+      bscTestnet: data.BscscanApiKey,
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 100000
+  }
+};
+
+export default config;
